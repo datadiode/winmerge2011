598cd009ca93c4772cada582ee804525daa3992b
admin <admin@jochen>
2013-02-18 08:44:18 +0100

    Changeset 297 (4ffa3119fa83) was lacking Merge7z/PostBuild.bat

---
+++ ArchiveSupport/Merge7z/PostBuild.bat
@@ -0,0 +1,19 @@
+@echo off
+
+if not exist %1 rem: goto :usage
+if not exist %2 rem: goto :usage
+
+set SevenZip=C:\7z920bin
+"%SevenZip%\7z.exe" e -y -o%~2\Merge7z\Lang %~1 *.ttt *.txt
+"%SevenZip%\7z.exe" e -so %~1 _7z.dll > %~2\Merge7z\7z.dll
+copy "%SevenZip%\7zip_pad.xml" %~2\Merge7z
+
+exit
+
+:usage
+echo ####################################################################
+echo # Merge7z.vcproj post-build script                                 #
+echo # Not intended for direct invocation through user interface        #
+echo # Post-build command line: PostBuild.bat "<msi_file>" "$(OutDir)"  #
+echo ####################################################################
+pause
