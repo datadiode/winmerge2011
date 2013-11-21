e78249e94b5728d28e16e8ec25d0738db454190b
jtuc <jtuc@localhost>
2013-11-21 08:10:21 +0100

    Create processor independent WinMergeU.exe.manifest and include in res/Merge.rc

--- Src/StdAfx.cpp
+++ Src/StdAfx.cpp
@@ -22,19 +22,11 @@
 //	Merge.pch will be the pre-compiled header
 //	stdafx.obj will contain the pre-compiled type information
 //
-// RCS ID line follows -- this is updated by CVS
-// $Id$
-
 #include "StdAfx.h"
 
-#if defined _M_IX86
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#elif defined _M_IA64
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#elif defined _M_X64
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#else
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#if _MSC_VER >= 1400
+#pragma comment(lib, "noarg.obj")
+#pragma comment(lib, "noenv.obj")
 #endif
 
 // Support EASTL as per <EASTL/allocator.h> line 194.
