eec169495b7e3f9364db8112941339dd14d46ae4
jtuc <jtuc@localhost>
2019-01-05 12:23:55 +0100

    ArchiveSupport: Upgrade to 7-Zip 18.06 & try AppVeyor build

--- appveyorbuild.cmd
+++ appveyorbuild.cmd
@@ -1,11 +1,34 @@
 setlocal
-for /f "usebackq tokens=*" %%i in (`"%programfiles(x86)%\microsoft visual studio\installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
-  set InstallDir=%%i
-)
-if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
-  call "%InstallDir%\Common7\Tools\vsdevcmd.bat
-)
+
+call "%VS140COMNTOOLS%\vsdevcmd.bat"
+
+REM Locate 7z.exe
+set $=%ProgramFiles%\7-zip;%ProgramFiles(x86)%\7-zip
+for %%$ in (7z.exe) do if not "%%~$$:$" == "" set SevenZip=%%~$$:$
 
 cd /d "%~dp0"
 
-MSBuild WinMerge.sln /t:Rebuild /p:Configuration="Release" /p:Platform="Win32" || pause
+mkdir appveyor-downloads
+
+for %%p in (
+  https://7-zip.org/a/7z1806.msi!7z1806.msi
+  https://7-zip.org/a/7z1806-x64.msi!7z1806-x64.msi
+  https://github.com/kornelski/7z/archive/18.06.zip!7z1806-src.zip
+) do (
+  for /F "tokens=1,2 delims=!" %%u in ("%%p") do (
+    if not exist appveyor-downloads\%%v (
+      powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest %%u -Outfile appveyor-downloads\%%v"
+    )
+  )
+)
+
+"%SevenZip%" x appveyor-downloads\*.zip -aoa -oappveyor-downloads\*
+
+set PathTo7zSrc=%~dp0appveyor-downloads\7z1806-src\7z-18.06
+set PathTo7zMsi=%~dp0appveyor-downloads
+
+for %%p in (%*) do (
+  for /F "tokens=1,2 delims=!" %%u in ("%%p") do (
+    MSBuild WinMerge.sln /t:Rebuild /p:Platform="%%u" /p:Configuration="%%v" || pause
+  )
+)
