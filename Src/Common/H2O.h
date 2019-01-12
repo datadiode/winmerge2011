dfbde3d7be6b47fe225662fad8346c111ebec7e7
jtuc <jtuc@localhost>
2019-01-12 17:45:22 +0100

    H2O2.h: New method ImageList::DrawEx()

--- Src/Common/H2O.h
+++ Src/Common/H2O.h
@@ -7,30 +7,7 @@
 // Copyright (c) 2005-2010  David Nash (as of Win32++ v7.0.2)
 // Copyright (c) 2011-2017  Jochen Neubeck
 //
-// Permission is hereby granted, free of charge, to
-// any person obtaining a copy of this software and
-// associated documentation files (the "Software"),
-// to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify,
-// merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom
-// the Software is furnished to do so, subject to the
-// following conditions:
-//
-// The above copyright notice and this permission notice
-// shall be included in all copies or substantial portions
-// of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
-// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
-// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-// OR OTHER DEALINGS IN THE SOFTWARE.
-//
+// SPDX-License-Identifier: MIT
 ////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
@@ -1118,6 +1095,10 @@ namespace H2O
 		BOOL Draw(int i, HDC hdc, int x, int y, UINT style)
 		{
 			return ::ImageList_Draw(m_hImageList, i, hdc, x, y, style);
+		}
+		BOOL DrawEx(int i, HDC hdc, int x, int y, int dx, int dy, COLORREF bk, COLORREF fg, UINT style)
+		{
+			return ::ImageList_DrawEx(m_hImageList, i, hdc, x, y, dx, dy, bk, fg, style);
 		}
 		BOOL Destroy()
 		{
