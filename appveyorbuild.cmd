709ea987f21e7254c1a23e41f9fbb2ec52a0c05a
jtuc <jtuc@localhost>
2019-05-26 10:18:42 +0200

    appveyorbuild.cmd: 19.00 not tagged (yet) at https://github.com/kornelski/7z

--- appveyorbuild.cmd
+++ appveyorbuild.cmd
@@ -11,9 +11,9 @@ cd /d "%~dp0"
 mkdir appveyor-downloads
 
 for %%p in (
-  https://7-zip.org/a/7z1806.msi!7z1806.msi
-  https://7-zip.org/a/7z1806-x64.msi!7z1806-x64.msi
-  https://github.com/kornelski/7z/archive/18.06.zip!7z1806-src.zip
+  https://7-zip.org/a/7z1900.msi!7z1900.msi
+  https://7-zip.org/a/7z1900-x64.msi!7z1900-x64.msi
+  https://github.com/kornelski/7z/archive/a6e2c7e401a3e5976e8522de518a169b0d8a7fac.zip!7z1900-src.zip
 ) do (
   for /F "tokens=1,2 delims=!" %%u in ("%%p") do (
     if not exist appveyor-downloads\%%v (
@@ -24,7 +24,7 @@ for %%p in (
 
 "%SevenZip%" x appveyor-downloads\*.zip -aoa -oappveyor-downloads\*
 
-set PathTo7zSrc=%~dp0appveyor-downloads\7z1806-src\7z-18.06
+set PathTo7zSrc=%~dp0appveyor-downloads\7z1900-src\7z-a6e2c7e401a3e5976e8522de518a169b0d8a7fac
 set PathTo7zMsi=%~dp0appveyor-downloads
 
 for %%p in (%*) do (
