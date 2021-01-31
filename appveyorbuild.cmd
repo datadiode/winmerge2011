2af3c6a3593a23e6d206da7fdd53b02c0897a854
datadiode <datadiode@users.noreply.github.com>
2021-01-31 13:18:51 +0100

    appveyorbuild.cmd

--- appveyorbuild.cmd
+++ appveyorbuild.cmd
@@ -14,6 +14,7 @@ for %%p in (
   https://7-zip.org/a/7z1900.msi!7z1900.msi
   https://7-zip.org/a/7z1900-x64.msi!7z1900-x64.msi
   https://github.com/kornelski/7z/archive/a6e2c7e401a3e5976e8522de518a169b0d8a7fac.zip!7z1900-src.zip
+  https://github.com/htacg/tidy-html5/archive/5.7.28.zip!tidy-html5.zip
   https://github.com/datadiode/winmerge2011/releases/download/0.2011.210.381/WinMerge_0.2011.210.381_setup.cpl!WinMerge_0.2011.210.381_setup.cpl
 ) do (
   for /F "tokens=1,2 delims=!" %%u in ("%%p") do (
@@ -27,6 +28,9 @@ for %%p in (
 
 set PathTo7zSrc=%~dp0appveyor-downloads\7z1900-src\7z-a6e2c7e401a3e5976e8522de518a169b0d8a7fac
 set PathTo7zMsi=%~dp0appveyor-downloads
+
+copy appveyor-downloads\tidy-html5.zip 3rdparty\
+call 3rdparty\configure.bat
 
 "%SevenZip%" x -oappveyor-downloads -t# "%~dp0appveyor-downloads\WinMerge_0.2011.210.381_setup.cpl" 1.7zSfxHtm.exe
 "%SevenZip%" x -oappveyor-downloads "%~dp0appveyor-downloads\1.7zSfxHtm.exe" DOWNLOADER
