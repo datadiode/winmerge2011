f29b5b1f6b0f687084a99ae6a4f9cb4c4b448705
datadiode <datadiode@users.noreply.github.com>
2021-05-17 08:11:24 +0200

    PostBuild.bat: Need to include ToxyExtract.config because of binding redirects

--- Src/PostBuild.bat
+++ Src/PostBuild.bat
@@ -162,7 +162,7 @@ for %%$ in (
 ) do if not "%%~$$:$" == "" (
 	xcopy /y "%%~$$:$\*.dll" "%~2ToxyExtract\bin\"
 	xcopy /y "%%~$$:$\*.pdb" "%~2ToxyExtract\bin\"
-	xcopy /y "%%~$$:$\*.exe" "%~2ToxyExtract\bin\"
+	xcopy /y "%%~$$:$\ToxyExtract.*" "%~2ToxyExtract\bin\"
 	xcopy /y "..\..\toxy\LICENSE" "%~2ToxyExtract\"
 	if errorlevel 1 copy /y ..\3rdparty\toxy_LICENSE "%~2ToxyExtract\LICENSE"
 	set $=
